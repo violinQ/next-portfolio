3316c91e187a7d71db34b1c51e72adce1c1c76fe
chentianlin <chentianlin@kingsoft.com>
2024-09-09 15:35:24 +0800

    project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import { ThemeProvider } from '@/components/ThemeProvider';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: "Chen's Portfolio",
+  description: 'Modern & Minimalist Next.js Personal Portfolio',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
